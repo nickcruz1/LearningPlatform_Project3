20ae953123effecb2c96d14fe5f3f7d66c1c083a
Nick Cruz <nickcruz920@gmail.com>
2023-02-27 16:11:07 -0500

    Creation of vue file

---
+++ index.vue
@@ -0,0 +1,12 @@
+<!-- This will be a Big Learning Platform (Portfolio project: 
+List of features:
+- A Quiz
+- Video tutorials/walkthrough of code
+- Changing one set of code to another language/different style of code (modeled after Vue)
+- Lots of Modals/Popups
+- Include Vue Router
+) -->
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    
